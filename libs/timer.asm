4af5b756c10299367ab9c600cce28aee53a8cf20
NihilisticPuffin <42104553+NihilisticPuffin@users.noreply.github.com>
2023-04-21 05:59:30 -0400

    Added Timer Library

---
+++ libs/timer.asm
@@ -0,0 +1,19 @@
+#register timer_start
+#register timer_end
+#register timer_elapsed
+
+; Calculates time in milliseconds between %timer_start and %timer_end being called
+; and stores the value in the timer_elapsed register
+
+%macro timer_start
+    utc timer_start
+%end
+
+%macro timer_end
+    ; Calculate time elapsed in milliseconds
+    utc timer_end
+    str timer_end
+    str timer_start
+    sub
+    ldr timer_elapsed
+%end
